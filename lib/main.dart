9d31cee96f9fe3f5b771c374bde9a3aae27af1aa
Kyu-u <melanchthonbutarbutar@gmail.com>
2020-11-25 16:28:13 +0700

    added anonsignin to test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tutor_me/pages/landing_page.dart';
 import 'package:tutor_me/pages/signUp1.dart';
 import 'package:tutor_me/pages/signUp2.dart';
 import 'package:tutor_me/pages/signin.dart';
+import 'package:tutor_me/services/auth..dart';
+import 'package:tutor_me/wrapper.dart';
+import 'package:tutor_me/models/user.dart';
 
-void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => SignUp1(),
-        '/signup1': (context) => SignUp1(),
-        '/signup2': (context) => SignUp2(),
-      },
-    ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
